bd10d2eadc3a7edf25c4caebe879de319d38103d
=Abdulrahman <=abdulrahman102034@gmail.com>
2021-12-17 23:10:20 +0200

    second commit

--- README.md
+++ README.md
@@ -2,12 +2,35 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+- [First](#First)
+- [Second](#Second)
 
-## Instructions
+### First
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+I made function called addItemToNav and in this function i did things
 
-To get started, open `js/app.js` and start building out the app's functionality
+**fist Thing:** i select the (ul) element which are inside Header element.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+**Second Thing:** after selecting, I created a (li)element inside the selector, inside this (ul)element,
+then by innerHTML i created a (a)element and this (a)element has (menu\_\_link)class for styles and has
+href attribute that it equal the (data-nav)class section to after clicking on item in navbar it going
+to this section in page.
+
+**Third Thing:** I made a loop to repeat the previous process.
+
+## Second
+
+I made function called addActiveClass and in this function i did things
+
+**First Thing:** I used if condition in conditiion i used getBoundingClientRect().top and getBondingClientRect().bottom to know this section in viewport or not.
+if it in viewport and if section don't have (your-active-class)class i will add this class for it and remove this class for another sections.
+
+**Second Thing:** i crate a loop for repeat this process.
+
+## Third
+
+call all function
+
+**First:** call first function (addItemToNav).
+
+**Second:** I created addEventListener to during scrolling call back (addActiveClass) function to add or remove (your-active-class)class.
--- js/app.js
+++ js/app.js
@@ -1,30 +1,21 @@
-/*
- * Define Global Variables
- * 
-*/
 const sections = document.querySelectorAll('section');
-/** 
- * End Global Variables
- * Start Helper Functions
- * 
-*/
 
-// This function for add item to navbar dynamically and on click it going to the section in page
+// This function for add item to navbar dynamically and on click it, it goes the section in page
 function addItemToNav() {
     // loop for every section i made it in navbar dynamically 
     for (section of sections) {
         document.querySelector('.navbar__menu').
             querySelector('ul')
             .appendChild(document.createElement('li'))
-            .innerHTML = `<li><a class="menu__link" href="#${section.getAttribute('id')}" >${section.getAttribute('data-nav')}</a></li>`
+            .innerHTML = `<a class="menu__link" href="#${section.getAttribute('id')}" >${section.getAttribute('data-nav')}</a>`
     }
 }
 
 // this function for know the section in the viewport or not and add your-active-class to the section or remove
 function addActiveClass() {
     for (section of sections) {
-        if (section.getBoundingClientRect().y <= 100 && section.getBoundingClientRect().bottom >= 200) {
-           if (!section.classList.contains('your-active-class')) {
+        if (section.getBoundingClientRect().y <= 100 && section.getBoundingClientRect().bottom >= 300) {
+            if (!section.classList.contains('your-active-class')) {
                 section.classList.add('your-active-class');
             }
         } else {
@@ -32,10 +23,9 @@ function addActiveClass() {
         }
     }
 }
-// End Helper Functions
 
 // build the nav
 addItemToNav();
 
-// Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', addActiveClass)
+//add the class (your-active-class)
+document.addEventListener('scroll', addActiveClass);
